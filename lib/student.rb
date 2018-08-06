292f8106003ba3be4851a0ef5634da7eca1a8be2
Vicki Hamm <vicki.cello@gmail.com>
2018-08-06 20:10:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,10 +41,10 @@ class Student
   end
 
   def self.create(name:, grade:)
-  student = Student.new(name, grade)
-  student.save
-  student
-end
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 
 
 end
