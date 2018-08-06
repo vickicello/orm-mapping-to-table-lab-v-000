e2555dc9cbe72149ff1f4266d8e485b21c45605d
Vicki Hamm <vicki.cello@gmail.com>
2018-08-06 20:03:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,5 +12,16 @@ class Student
     @id = some_id
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+        SQL
+      DB[:conn].execute(sql)
+  end
+
 
 end
